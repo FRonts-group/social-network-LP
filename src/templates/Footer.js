6c8c4a8b443746e3b82ccf39f2554d44d0d3e36e
rodrigojgq <rodrigojgq@gmail.com>
2021-06-08 19:58:06 -0400

    Add Footer Template

---
+++ src/templates/Footer.js
@@ -0,0 +1,19 @@
+const Footer = () => {
+  const view = `
+    <footer>
+      <div class="footerMenu">
+        <a href="#">INICIO</a>
+        <a href="#">INICIAR SESIÓN</a>
+        <a href="#">REGISTRARSE</a>
+      </div>
+      <p id="collaborators">Made with ♥ by <a href="https://github.com/fabiola29298">Fabiola</a> and <a
+          href="https://github.com/rodrigojgq">Rodrigo</a> </p>
+      <a  href="https://github.com/FRonts-group/social-network-LP" id="repository">
+        <span class="logo-container"></span> Check out our repository in Github
+      </a>
+    </footer>
+  `;
+  return view;
+};
+
+export default Footer;
